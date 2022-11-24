ab7ee4dc3b2cd0d60cc2b071fe2b0f794a50f22a
Andrey Gorelov <shorkun1974@yandex.ru>
2022-11-24 15:05:00 +0300

    первый коммит

--- Module 8-7.cpp
+++ Module 8-7.cpp
@@ -11,3 +11,42 @@
 
 Что оценивается
 Правильно определяется количество комбинаций.*/
+
+#include <cmath>
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //затухание маятника
+    float attenuation = 0.084f;
+
+    std::cout << "Введите начальную амплитуду маятника в сантиметрах: ";
+    float startAmplitude;
+    std::cin >> startAmplitude;
+    while (startAmplitude <= 0) {
+        std::cout << "Амплитуда должна быть больше 0. Введите снова: ";
+        std::cin >> startAmplitude;
+    }
+
+    std::cout << "Введите конечную амплитуду маятника в сантиметрах: ";
+    float stopAmplitude;
+    std::cin >> stopAmplitude;
+    while (stopAmplitude <= 0) {
+        std::cout << "Амплитуда должна быть больше 0. Введите снова: ";
+        std::cin >> stopAmplitude;
+    }
+
+    float currentAmplitude = startAmplitude;
+    int numberSwings = 0;
+
+    while (currentAmplitude > stopAmplitude) {
+        currentAmplitude -= currentAmplitude * attenuation;
+        numberSwings++;
+    }
+
+    std::cout << "Количество качаний до остановки маятника: " << numberSwings;
+}
